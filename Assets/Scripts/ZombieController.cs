44a30a0c97a023c5507c0e2ab5b00fc9863ffb5a
Lucas Primo <lucas.primo@bradesco.com.br>
2021-01-10 16:59:11 -0300

    Zombie improvements

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnAnimatorMove()
+    {
+        agent.velocity = anim.deltaPosition / Time.deltaTime;
+    }
+}
+
